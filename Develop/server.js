f991ff42bca0a09e2d7d90c4e21cfd3b6cdb0490
Cristina <cristinayunesm@gmail.com>
2022-09-22 18:23:00 -0400

    Fixed error on server.js static public missing

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-// const path = require('path');
-// const { clog } = require('./middleware/clog');
 const apiroutes = require('./routes/apiroutes.js');
 const htmlroutes = require('./routes/htmlroutes.js');
 
@@ -8,14 +6,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-// app.use(clog);
 
-// Middleware for parsing JSON and urlencoded form data
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/api', apiroutes);
 app.use('/', htmlroutes)
-app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
